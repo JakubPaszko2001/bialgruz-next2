e8c67eb372c74aed98874769b45758feb81071c3
P4CHO <fraymweb@gmail.com>
2026-09-15 23:32:32 +0200

    Sekcja dlaczego my - przesuniecia obrazka hover w vw dla mobile/tablet

--- components/subUI.jsx
+++ components/subUI.jsx
@@ -200,26 +200,26 @@ export function WhySection({ title, features, image }) {
           </div>
           <Reveal delay={0.2} className="group relative">
             <div className="absolute inset-[-40px] bg-[radial-gradient(ellipse,rgba(207,220,0,0.06)_0%,transparent_70%)]" />
-            <div className="relative mx-auto w-full max-w-[400px] -translate-x-[30px] md:translate-x-0">
-              <Image
-                src={image.src}
-                alt={image.alt}
-                width={400}
-                height={520}
-                className={`relative block w-full object-contain drop-shadow-[0_30px_80px_rgba(0,0,0,0.5)] transition-opacity duration-300 ${
-                  image.hoverSrc ? "group-hover:opacity-0" : ""
-                }`}
-              />
-              {image.hoverSrc && (
-                <Image
-                  src={image.hoverSrc}
-                  alt={image.alt}
-                  width={400}
-                  height={520}
-                  className="absolute inset-0 block w-full translate-x-[67px] object-contain opacity-0 drop-shadow-[0_30px_80px_rgba(0,0,0,0.5)] transition-opacity duration-300 group-hover:opacity-100 md:translate-x-[77px]"
-                />
-              )}
-            </div>
+                        <div className="relative mx-auto w-full max-w-[400px] -translate-x-[8vw] lg:translate-x-0">
+                          <Image
+                            src={image.src}
+                            alt={image.alt}
+                            width={400}
+                            height={520}
+                            className={`relative block w-full object-contain drop-shadow-[0_30px_80px_rgba(0,0,0,0.5)] transition-opacity duration-300 ${
+                              image.hoverSrc ? "group-hover:opacity-0" : ""
+                            }`}
+                          />
+                          {image.hoverSrc && (
+                            <Image
+                              src={image.hoverSrc}
+                              alt={image.alt}
+                              width={400}
+                              height={520}
+                              className="absolute inset-0 block w-full translate-x-[18vw] object-contain opacity-0 drop-shadow-[0_30px_80px_rgba(0,0,0,0.5)] transition-opacity duration-300 group-hover:opacity-100 sm:translate-x-[14vw] md:translate-x-[10vw] lg:translate-x-[77px]"
+                            />
+                          )}
+                        </div>
           </Reveal>
         </div>
       </div>
